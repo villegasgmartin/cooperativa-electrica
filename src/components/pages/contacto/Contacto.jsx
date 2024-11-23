656df3e5392f78effbedd35f0ee6110b1a40930e
Aurelio Puente <aurelianopuente@hotmail.com>
2024-11-23 04:59:53 -0300

    aplicando diseño

--- src/components/pages/contacto/Contacto.jsx
+++ src/components/pages/contacto/Contacto.jsx
@@ -89,24 +89,25 @@ const Contacto = () => {
 
   return (
     <section className='contacto-main-container'>
-      <div className='contacto-mensaje-container'> 
-        <Fade triggerOnce={true} duration={800} delay={300} direction='left'>
-          <div className='contacto-mensaje-electrica'>
-            <h3 className='contacto-number'>Guardia técnica Servicio Eléctrico</h3>
-            <p className='contacto-mensaje'>Solo para EMERGENCIAS (Corresponde a casos de Personas Electrodependientes)</p>
-            <h3 className='contacto-number'>0800-3330357</h3>
-          </div>
-        </Fade>
-        <Fade triggerOnce={true} duration={800} delay={300} direction='right'>
-            <div className='contacto-mensaje-nave'>
-              <h3 className='contacto-number'>Guardia técnica NAVE Internet</h3>
-              <p className='contacto-mensaje'>De Lunes a Viernes de 7 a 23hs (Solo llamados)</p>
-              <h3 className='contacto-number'>153060280</h3>
-            </div>
-        </Fade>
-      </div>
-      <Fade triggerOnce={true} duration={800} delay={300}>
+        <div className='contacto-container'> 
+          <div className='contacto-mensaje-container'> 
+            <Fade triggerOnce={true} duration={800} delay={300} direction='left'>
+              <div className='contacto-mensaje-electrica'>
+                <h3 className='contacto-number'>Guardia técnica Servicio Eléctrico</h3>
+                <p className='contacto-mensaje'>Solo para EMERGENCIAS (Corresponde a casos de Personas Electrodependientes)</p>
+                <h3 className='contacto-number'>0800-3330357</h3>
+              </div>
+            </Fade>
+            <Fade triggerOnce={true} duration={800} delay={300} direction='right'>
+                <div className='contacto-mensaje-nave'>
+                  <h3 className='contacto-number'>Guardia técnica NAVE Internet</h3>
+                  <p className='contacto-mensaje'>De Lunes a Viernes de 7 a 23hs (Solo llamados)</p>
+                  <h3 className='contacto-number'>153060280</h3>
+                </div>
+            </Fade>
+        </div>
         <form onSubmit={handleSubmit} className='contacto-form-container'>
+          <h2 className='contacto-form-title'>¿En qué podemos ayudarte?</h2>
           <TextField
             label="Nombre"
             name="nombre"
@@ -117,8 +118,8 @@ const Contacto = () => {
             value={formData.nombre}
             onChange={handleChange}
             sx={{
-              backgroundColor: "#d9f3e3",
               '& .MuiOutlinedInput-root': {
+                borderRadius: "15px",
                 '&.Mui-focused fieldset': {
                   borderColor: '#12824c',
                 },
@@ -128,7 +129,6 @@ const Contacto = () => {
               }
             }}
           />
-
           <TextField
             label="Correo electrónico"
             name="correo"
@@ -140,8 +140,8 @@ const Contacto = () => {
             value={formData.correo}
             onChange={handleChange}
             sx={{
-              backgroundColor: "#d9f3e3",
               '& .MuiOutlinedInput-root': {
+                borderRadius: "15px",
                 '&.Mui-focused fieldset': {
                   borderColor: '#12824c',
                 },
@@ -159,7 +159,7 @@ const Contacto = () => {
               onChange={handleChange}
               required
               variant='outlined'
-              sx={{ backgroundColor: "#d9f3e3" }}
+              sx={{ borderRadius: "15px" }}
             >
               <MenuItem value="Consulta sobre NAVE">Consulta sobre NAVE</MenuItem>
               <MenuItem value="Consulta sobre Electricidad">Consulta sobre Electricidad</MenuItem>
@@ -178,8 +178,8 @@ const Contacto = () => {
             value={formData.mensaje}
             onChange={handleChange}
             sx={{
-              backgroundColor: "#d9f3e3",
               '& .MuiOutlinedInput-root': {
+                borderRadius: "15px",
                 '&.Mui-focused fieldset': {
                   borderColor: '#12824c',
                 },
@@ -189,14 +189,16 @@ const Contacto = () => {
               }
             }}
           />
-
           <div className='contactos-button-container'>
             <Button
               sx={{
                 width: "100%",
-                height: "60px",
-                fontFamily: "archivo",
-                backgroundColor: "#12824c",
+                height: "100%",
+                fontFamily: "interTight",
+                fontSize: "25px",
+                textTransform: "none",
+                borderRadius: "25px",
+                backgroundColor: "#8048ff",
                 marginTop: "20px"
               }}
               variant='contained'
@@ -207,8 +209,7 @@ const Contacto = () => {
             </Button>
           </div>
         </form>
-      </Fade>
-
+        </div>
       <Fade triggerOnce={true} duration={800} delay={300} direction='up'>
       <div className='contacto-info-container'>
           <div className='contacto-info'>
@@ -217,23 +218,28 @@ const Contacto = () => {
               <p className='contacto-info-text'><strong>Área comercial: </strong>comercial@coopelectmdp.com.ar</p>
               <p className='contacto-info-text'><strong>Horarios de atención: </strong>Lunes a Viernes de 7:30 a 12:30 hs.</p>
               <p className='contacto-info-text'><strong>Consejo de Administración: </strong>consejo@coopelectmdp.com.ar</p>
-              <a href="https://www.facebook.com/people/Cooperativa-El%C3%A9ctrica-MDP-Nave-Internet/61565189905138/" target='_blank' rel='noopener noreferrer'>
-                <FacebookIcon
-                  sx={{
-                    color: "#12824c",
-                    fontSize: {
-                      xs: '50px',
-                      sm: '60px',
-                    },
-                    '&:hover': { transform: 'scale(1.2)' },
-                    transition: 'transform 0.3s ease, color 0.3s ease',
-                  }}
-                />
-              </a>
+              <div className='contacto-icons'>
+                <a href="https://www.facebook.com/people/Cooperativa-El%C3%A9ctrica-MDP-Nave-Internet/61565189905138/" target='_blank' rel='noopener noreferrer'>
+                  <FacebookIcon
+                    sx={{
+                      color: "#12824c",
+                      fontSize: {
+                        xs: '90px',
+                        sm: '90px', 
+                        md: '90px', 
+                        lg: '90px',
+                        xl: '90px', 
+                      },
+                      '&:hover': { transform: 'scale(1.2)' },
+                      transition: 'transform 0.3s ease, color 0.3s ease',
+                    }}
+                  />
+                </a>
+              </div>
             </div>
           </div>
           <div className='contacto-info'>
-            <h4 className='contacto-info-title'>NAVE Internet</h4>
+            <h4 className='contacto-info-title contacto-color'>NAVE Internet</h4>
             <div className='contacto-info-align'>
               <p className='contacto-info-text'><strong>Área comercial: </strong>nave@cooperativamdp.com.ar</p>
               <p className='contacto-info-text'><strong>Horarios de atención: </strong>Lunes a Viernes de 8 a 15 hs.</p>
@@ -242,10 +248,13 @@ const Contacto = () => {
                 <a href="https://www.facebook.com/naveinternetoficial" target='_blank' rel='noopener noreferrer'>
                   <FacebookIcon
                     sx={{
-                      color: "#12824c",
+                      color: "#3d116d",
                       fontSize: {
-                        xs: '50px',
-                        sm: '60px',
+                        xs: '90px',
+                        sm: '90px', 
+                        md: '90px', 
+                        lg: '90px', 
+                        xl: '90px',
                       },
                       '&:hover': { transform: 'scale(1.2)' },
                       transition: 'transform 0.3s ease, color 0.3s ease',
@@ -255,10 +264,13 @@ const Contacto = () => {
                 <a href="https://www.instagram.com/coopelectmdp_nave/" target='_blank' rel='noopener noreferrer'>
                   <InstagramIcon
                     sx={{
-                      color: "#12824c",
+                      color: "#3d116d",
                       fontSize: {
-                        xs: '50px',
-                        sm: '60px',
+                        xs: '90px',
+                        sm: '90px',
+                        md: '90px', 
+                        lg: '90px', 
+                        xl: '90px', 
                       },
                       '&:hover': { transform: 'scale(1.2)' },
                       transition: 'transform 0.3s ease, color 0.3s ease',
@@ -272,6 +284,6 @@ const Contacto = () => {
       </Fade>
     </section>
   );
-};
+  };
 
 export default Contacto;
--- src/components/pages/formasPago/FormasPago.jsx
+++ src/components/pages/formasPago/FormasPago.jsx
@@ -5,6 +5,12 @@ import { setTitle } from '../../../store/titleSlice';
 import PagosAcordeon from '../../common/FormasPagoComponents/PagosAcordeon';
 import "../formasPago/FormasPago.css"
 import { Fade } from 'react-awesome-reveal';
+import Image01 from "../../../assets/images/pagos/pagos-01.png"
+import Image02 from "../../../assets/images/pagos/pagos-02.png"
+import Image03 from "../../../assets/images/pagos/pagos-03.png"
+import Image04 from "../../../assets/images/pagos/pagos-04.png"
+import Image05 from "../../../assets/images/pagos/pagos-05.png"
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 //JSX:
 const FormasPago = () => {
@@ -17,42 +23,55 @@ const FormasPago = () => {
 
   return (
     <section className='pagos-main-container'>
+      <Fade triggerOnce={true} duration={800} delay={300} direction='right'>
+        <div className='pagos-logos-container'>
+          <img src={Image01} alt="BNA+"  className='pagos-logos'/>
+          <img src={Image02} alt="Cuenta DNI"  className='pagos-logos'/>
+          <img src={Image03} alt="Mercado Pagp"  className='pagos-logos'/>
+          <img src={Image04} alt="MODO"  className='pagos-logos'/>
+          <img src={Image05} alt="Provincia Net Pagos"  className='pagos-logos'/>
+        </div>
+      </Fade>
       <div className='pagos-container'>
-        <div className='logos-container'>
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
-        </div>
-        <h2>¡Pagá tus impuestos y servicios!</h2>
-        <p><strong>Extraé dinero</strong> en efectivo con tu tarjeta de débito del Banco Provincia</p>
-          <p><strong>Pagá con tarjeta de débito</strong> del Banco Provincia</p>
-        <div>
-          <p>Horario: de 7:30 a 12:30 hs</p>
-          <p> Para más información entrá en la página web oficial de Provincia NET www.provincianet.com.ar</p>
-        </div>
-        <div>
-          <p>ALIAS;</p>
-          <ul>
-            <li>COOP-PROVINCIA </li>
-            <li>COOP-NACION</li>
-          </ul>
-      </div>
         <Fade triggerOnce={true} duration={800} delay={300} direction='left'>
+          <h2 className='pagos-title'>¡Pagá tus impuestos y servicios!</h2>
+          <div className='pagos-arrow-text'> 
+            <ArrowRightAltIcon sx={{fontSize: "35px", color: "#3d116d;"}}/><p className='pagos-text'><strong>Extraé dinero</strong> en efectivo con tu tarjeta de débito del Banco Provincia</p>
+          </div>
+          <div className='pagos-arrow-text'> 
+            <ArrowRightAltIcon sx={{fontSize: "35px", color: "#3d116d;"}}/><p className='pagos-text'><strong>Pagá con tarjeta de débito </strong>del Banco Provincia</p>
+          </div>
+        </Fade>
+          <Fade triggerOnce={true} duration={800} delay={300}>
+            <p className='pagos-text-02'>Horario: de 7:30 a 12:30 hs<br/>Para más información entrá en la página web oficial de Provincia NET www.provincianet.com.ar</p>
+            <div className='pagos-alias-container'>
+              <p className='pagos-text-03'>ALIAS:</p>
+              <ul>
+                <li className='pagos-text-03'>COOP-PROVINCIA </li>
+                <li className='pagos-text-03'>COOP-NACION</li>
+              </ul>
+            </div>
+          </Fade>
+        <Fade triggerOnce={true} duration={800} delay={300} direction='up'>
           <div className='pagos-accordion-container'><PagosAcordeon/></div>
         </Fade>
       </div>
-        <div className='pagos-info-container'>
-          <div className='pagos-usuarios-container'>
-            <Fade triggerOnce={true} duration={800} delay={300} direction='up'>
-              <p className='pagos-info-text'><span style={{color: "#12824c", fontWeight: "bold"}}>Sres. Usuarios</span><br/>
-                En caso de <strong>corte de servicio por falta de pago</strong>, si abonan el valor de la reconexión por transferencia o depósito, <strong>dicha reconexión de servicio se hará efectiva una vez que se acredite el pago en nuestros bancos</strong>.Recuerde enviar el comprobante de pago por mail indicando el N° de asociado, como indican las instrucciones.<br/>
-                Muchas gracias.
-              </p>
-              <p className='pagos-info-text' ><span style={{color: "#12824c", fontWeight: "bold"}}>Sres. /as. Asociados:<br/></span> Se informa que a partir de abril 2023  la emisión de facturas es mensual, a fin de facilitar el pago del servicio eléctrico frente al aumento de los cuadros tarifarios, para dar cumplimiento a lo dispuesto oportunamente por el Organismo de Control Eléctrico de la Provincia de Buenos Aires OCEBA.
-              </p>
+        <div className='pagos-usuarios-container'>
+            <Fade triggerOnce={true} duration={800} delay={300} direction='left'>
+              <div className='pagos-usuarios'>
+                <p className='pagos-info-title'>Sres. Usuarios</p>
+                <p className='pagos-info-text'>En caso de corte de servicio por falta de pago, si abonan el valor de la reconexión por transferencia o depósito, dicha reconexión de servicio se hará efectiva una vez que se acredite el pago en nuestros bancos. Recuerde enviar el comprobante de pago por mail indicando el N° de asociado, como indican las instrucciones.<br/>
+                  Muchas gracias.
+                </p>
+              </div>
             </Fade>
-          </div>
+            <Fade  triggerOnce={true} duration={800} delay={300} direction='right'>
+              <div className='pagos-usuarios pagos-background'>
+                <p className='pagos-info-title' >Sres. /as. Asociados:</p>  
+                <p  className='pagos-info-text'>Se informa que a partir de abril 2023  la emisión de facturas es mensual, a fin de facilitar el pago del servicio eléctrico frente al aumento de los cuadros tarifarios, para dar cumplimiento a lo dispuesto oportunamente por el Organismo de Control Eléctrico de la Provincia de Buenos Aires OCEBA.
+                </p>
+              </div>
+            </Fade>
         </div>
     </section>
   );
