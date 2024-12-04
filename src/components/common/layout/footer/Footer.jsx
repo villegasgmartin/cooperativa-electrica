9274511000a9cb2af461ce7885d1fed143c75f6b
Aurelio Puente <aurelianopuente@hotmail.com>
2024-12-04 14:37:48 -0300

    corrección teléfonos en internet

--- src/components/common/layout/footer/Footer.jsx
+++ src/components/common/layout/footer/Footer.jsx
@@ -1,17 +1,20 @@
-//Importaciones:
-import { Link } from "react-router-dom"
-import FooterLogo from "../../../../assets/images/logos/logo.png"
-import "../footer/Footer.css"
+// Importaciones necesarias
+import { Link, useLocation } from "react-router-dom";
+import FooterLogo from "../../../../assets/images/logos/logo.png";
+import "../footer/Footer.css";
 
-//JSX:
 const Footer = () => {
-
     const currentYear = new Date().getFullYear();
+    const location = useLocation();
+
+    // Verifica si la ruta actual es "/nave" o "/formulario"
+    const excludePages = ["/nave", "/formulario"];
+    const shouldHideSpecificInfo = excludePages.includes(location.pathname);
 
     return (
         <footer className="footer-container">
             <div className="footer-logo-container">
-                <img className="footer-logo" src={FooterLogo} alt="" />
+                <img className="footer-logo" src={FooterLogo} alt="Logo" />
             </div>
             <div className="footer-copy-links">
                 <div className="footer-links-container">
@@ -25,6 +28,8 @@ const Footer = () => {
                 </div>
                 <p className="copyright">Copyright (c)<span>{currentYear}</span> Desarrollado por <a className="copyright-link" href="https://duwohdevelopers.com/" target="_blank">Duwoh Developers</a></p>
             </div>
+
+            {/* Bloques de información específicos */}
             <div className="footer-info-container">
                 <div>
                     <h4 className="footer-info-title">TELEFONOS:</h4>
@@ -38,20 +43,32 @@ const Footer = () => {
                     <h4 className="footer-info-title">HORARIOS DE ATENCIÓN NAVE INTERNET:</h4>
                     <p className="footer-info">Lunes a Viernes de 8 a 15 hs.</p>
                 </div>
-                <div>
-                    <h4 className="footer-info-title">HORARIOS DE ATENCIÓN SERVICIO ELÉCTRICO:</h4>
-                    <p className="footer-info">Lunes a Viernes de 7:30 a 12:30 hs.</p>
-                </div>
-                <div>
-                    <h4 className="footer-info-title">GUARDIA SERVICIO ELÉCTRICO:</h4>
-                    <a href="tel:2235353648" style={{textDecoration: "none"}}><p className="footer-info">223 535-3648</p></a>
-                    <a href="tel:2235354042" style={{textDecoration: "none"}}><p className="footer-info">223 535-4042</p></a>
-                    <a href="tel:2235351358" style={{textDecoration: "none"}}><p className="footer-info">223 535-1358</p></a>
-                </div>
+                {/* Ocultar estos divs solo en /nave y /formulario */}
+                {!shouldHideSpecificInfo && (
+                    <>
+                        <div>
+                            <h4 className="footer-info-title">HORARIOS DE ATENCIÓN SERVICIO ELÉCTRICO:</h4>
+                            <p className="footer-info">Lunes a Viernes de 7:30 a 12:30 hs.</p>
+                        </div>
+                        <div>
+                            <h4 className="footer-info-title">GUARDIA SERVICIO ELÉCTRICO:</h4>
+                            <a href="tel:2235353648" style={{ textDecoration: "none" }}>
+                                <p className="footer-info">223 535-3648</p>
+                            </a>
+                            <a href="tel:2235354042" style={{ textDecoration: "none" }}>
+                                <p className="footer-info">223 535-4042</p>
+                            </a>
+                            <a href="tel:2235351358" style={{ textDecoration: "none" }}>
+                                <p className="footer-info">223 535-1358</p>
+                            </a>
+                        </div>
+                    </>
+                )}
+                {/* Resto de la información que siempre se muestra */}
             </div>
             <p className="copyright copy-movile">Copyright (c)<span>{currentYear}</span> Desarrollado por <a className="copyright-link" href="https://duwohdevelopers.com/" target="_blank">Duwoh Developers</a></p>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
