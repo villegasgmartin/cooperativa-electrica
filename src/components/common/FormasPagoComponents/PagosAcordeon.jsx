d785b7e09730e790c9bf9ad59a0875c791da6b0b
MartinLuxury <166442590+MartinLuxury@users.noreply.github.com>
2025-04-23 21:37:00 +0200

    metodos de pago links

--- src/components/common/FormasPagoComponents/PagosAcordeon.jsx
+++ src/components/common/FormasPagoComponents/PagosAcordeon.jsx
@@ -49,7 +49,6 @@ export default function PagosAcordeon() {
             <ul>
               <li>Seleccione el rubro: Luz, Agua, Gas</li>
               <li>Seleccione el ente: Coop Electricidad MDP</li>
-              <li>Ingrese su código de pagos.</li>
               <li className='FormasPago-accordion-content'>Ingresá el código de pagos <strong>( ¿donde encuentro mi código de pagos? haga click <a href="https://res.cloudinary.com/dj3akdhb9/image/upload/v1728488740/cooperativa/Click_para_ver_factura_de_energia_yef0tt.png" target='_blank'  className='FormasPago-accordion-link'>aquí para Servicio Eléctrico</a> y <a href="https://res.cloudinary.com/dj3akdhb9/image/upload/v1728488740/cooperativa/Click_para_ver_factura_de_NAVE_qclwju.png" target='_blank' className='FormasPago-accordion-link'>aquí para Nave Internet</a> )</strong></li>
             </ul>
 
