1627d4f1871088d9409a1101eba0c808028748e0
MartinLuxury <166442590+MartinLuxury@users.noreply.github.com>
2025-05-08 15:55:05 +0200

    incluir formosa

--- src/components/pages/formulario/Form.jsx
+++ src/components/pages/formulario/Form.jsx
@@ -100,8 +100,8 @@ const Form = () => {
 
         { latitude: -38.013, longitude: -57.56665 },
         { latitude: -38.01212, longitude: -57.5686 },
-        { latitude: -38.01279, longitude: -57.56914 },
-        { latitude: -38.01645, longitude: -57.56206 },
+        { latitude: -38.01295, longitude: -57.5692 },
+        { latitude: -38.0165, longitude: -57.56215 },
         { latitude: -38.01325, longitude: -57.55959 },
         { latitude: -38.01063, longitude: -57.56476 },
   
