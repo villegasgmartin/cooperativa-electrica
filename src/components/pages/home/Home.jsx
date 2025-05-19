b4d8a19d09d680807e866b0849368898bb4d7294
MartinLuxury <166442590+MartinLuxury@users.noreply.github.com>
2025-05-19 13:22:18 +0200

    sacar banner promocion

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -4,41 +4,41 @@ import HomeServices3 from "../../common/HomeComponents/homeServices3/HomeService
 import HomeGuardia2 from "../../common/HomeComponents/homeGuardia2/HomeGuardia2";
 import HomeBlog2 from "../../common/HomeComponents/homeBlog2/HomeBlog2";
 import Footer from "../../common/layout/footer/Footer";
-import ModalHome from "../../../assets/images/oferta300MB.jpg"
+// import ModalHome from "../../../assets/images/oferta300MB.jpg"
 /*import { useState, useEffect } from "react"*/
 import "../home/Home.css"
 import BotonWhatsapp from "../../common/BotonWhatsapp/BotonWhatsapp";
-import { useEffect, useState } from "react";
-import { Link } from "react-router";
-import { Button } from "@mui/material";
+// import { useEffect, useState } from "react";
+// import { Link } from "react-router";
+// import { Button } from "@mui/material";
 
 //JSX:
 const Home = () => {
 
 
-    const [showImageModal, setShowImageModal] = useState(false);
+    // const [showImageModal, setShowImageModal] = useState(false);
 
-    useEffect(() => {
-        const hasShownAlert = sessionStorage.getItem("homeAlertShown");
+    // useEffect(() => {
+    //     const hasShownAlert = sessionStorage.getItem("homeAlertShown");
 
-        if (!hasShownAlert) {
-            const timer = setTimeout(() => {
-                setShowImageModal(true);
-                sessionStorage.setItem("homeAlertShown", "true");
-            }, 1000);
-            return () => clearTimeout(timer);
-        }
-    }, []);
+    //     if (!hasShownAlert) {
+    //         const timer = setTimeout(() => {
+    //             setShowImageModal(true);
+    //             sessionStorage.setItem("homeAlertShown", "true");
+    //         }, 1000);
+    //         return () => clearTimeout(timer);
+    //     }
+    // }, []);
 
-    const closeModal = () => {
-        setShowImageModal(false);
-    };
+    // const closeModal = () => {
+    //     setShowImageModal(false);
+    // };
 
 
     return (
         <div>
             
-            {showImageModal && (
+            {/* {showImageModal && (
             <div className="modal-overlay" onClick={closeModal}>
             <div className="modal-content">
                 <img
@@ -70,7 +70,7 @@ const Home = () => {
                 </Button>
             </div>
             </div>
-        )}
+        )} */}
            
             <HomePortada2/>
             <HomeServices3/>
