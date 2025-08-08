0735ec9075040d7271d1bfd189006766701dcc93
MartinLuxury <166442590+MartinLuxury@users.noreply.github.com>
2025-08-08 18:31:27 +0200

    popup home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -4,72 +4,57 @@ import HomeServices3 from "../../common/HomeComponents/homeServices3/HomeService
 import HomeGuardia2 from "../../common/HomeComponents/homeGuardia2/HomeGuardia2";
 import HomeBlog2 from "../../common/HomeComponents/homeBlog2/HomeBlog2";
 import Footer from "../../common/layout/footer/Footer";
-// import ModalHome from "../../../assets/images/oferta300MB.jpg"
+import ModalHome from "../../../assets/images/logos/logo.png"
 /*import { useState, useEffect } from "react"*/
 import "../home/Home.css"
-// import { useEffect, useState } from "react";
-// import { Link } from "react-router";
-// import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+import { Button } from "@mui/material";
 
 //JSX:
 const Home = () => {
 
 
-    // const [showImageModal, setShowImageModal] = useState(false);
+    const [showImageModal, setShowImageModal] = useState(false);
 
-    // useEffect(() => {
-    //     const hasShownAlert = sessionStorage.getItem("homeAlertShown");
+    useEffect(() => {
+        const hasShownAlert = sessionStorage.getItem("homeAlertShown");
 
-    //     if (!hasShownAlert) {
-    //         const timer = setTimeout(() => {
-    //             setShowImageModal(true);
-    //             sessionStorage.setItem("homeAlertShown", "true");
-    //         }, 1000);
-    //         return () => clearTimeout(timer);
-    //     }
-    // }, []);
+        if (!hasShownAlert) {
+            const timer = setTimeout(() => {
+                setShowImageModal(true);
+                sessionStorage.setItem("homeAlertShown", "true");
+            }, 3000);
+            return () => clearTimeout(timer);
+        }
+    }, []);
 
-    // const closeModal = () => {
-    //     setShowImageModal(false);
-    // };
+    const closeModal = () => {
+        setShowImageModal(false);
+    };
 
 
     return (
         <div>
             
-            {/* {showImageModal && (
+            {showImageModal && (
             <div className="modal-overlay" onClick={closeModal}>
             <div className="modal-content">
-                <img
-                src={ModalHome}
-                alt="modal del inicio"
-                className="modal-image"
-                />
-                <Button
-                  component={Link}
-                  to="/formulario"
-                  sx={{
-                    width: "100%",
-                    height: "40px",
-                    fontFamily: "interTight",
-                    marginTop: "10px",
-                    fontSize: "20px",
-                    fontWeight: "bold",
-                    letterSpacing: "2px",
-                    borderRadius: "30px",
-                    boxShadow: "8px 8px 8px rgba(0, 0, 0, 0.3)",
-                    textTransform: "none",
-                    color: "white",
-                    backgroundColor: "#8048ff",
-                  }}
-                  variant="contained"
-                  size="large"
-                >
-                  Contratá
-                </Button>
+              <img src={ModalHome} alt="" className="modal-image"/>
+            <p className="modal-title">Estimado usuario: le informamos que incorporamos los siguientes números telefónicos para el envío de facturas e información institucional por WhatsApp 
+            </p>
+          <div>
+           <a href="https://api.whatsapp.com/send?phone=542233060278" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+                            <p className="footer-info">223-3060278</p></a>
+                                       <a href="https://api.whatsapp.com/send?phone=542233060273" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+                            <p className="footer-info">223-3060273</p></a>
+                                       <a href="https://api.whatsapp.com/send?phone=542234060274" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+                            <p className="footer-info">223-4060274</p></a>
+                  
+          </div>
             </div>
             </div>
-        )} */}
+        )}
             <HomePortada2/>
             <HomeServices3/>
             <HomeGuardia2/>
