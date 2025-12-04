6065084de9788ce3df3f5f88be74145abcd189e4
MartinLuxury <166442590+MartinLuxury@users.noreply.github.com>
2025-12-04 14:05:05 +0100

    sacar modal home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -17,42 +17,42 @@ const Home = () => {
 
     const [showImageModal, setShowImageModal] = useState(false);
 
-    useEffect(() => {
-        const hasShownAlert = sessionStorage.getItem("homeAlertShown");
+    // useEffect(() => {
+    //     const hasShownAlert = sessionStorage.getItem("homeAlertShown");
 
-        if (!hasShownAlert) {
-            const timer = setTimeout(() => {
-                setShowImageModal(true);
-                sessionStorage.setItem("homeAlertShown", "true");
-            }, 3000);
-            return () => clearTimeout(timer);
-        }
-    }, []);
+    //     if (!hasShownAlert) {
+    //         const timer = setTimeout(() => {
+    //             setShowImageModal(true);
+    //             sessionStorage.setItem("homeAlertShown", "true");
+    //         }, 3000);
+    //         return () => clearTimeout(timer);
+    //     }
+    // }, []);
 
-    const closeModal = () => {
-        setShowImageModal(false);
-    };
+    // const closeModal = () => {
+    //     setShowImageModal(false);
+    // };
 
 
     return (
         <div>
             
-            {showImageModal && (
-            <div className="modal-overlay" onClick={closeModal}>
-                <div className="modal-content">
-                    <img src={ModalHome} alt="" className="modal-image"/>
-                <p className="modal-title">Estimado usuario: le informamos que incorporamos los siguientes números telefónicos para el envío de facturas e información institucional por WhatsApp 
-                </p>
-                    <div>
-                        <a href="https://api.whatsapp.com/send?phone=542233060278" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
-                            <p className="footer-info">223-3060278</p></a>
-                                <a href="https://api.whatsapp.com/send?phone=542233060273" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
-                            <p className="footer-info">223-3060273</p></a>
-                                <a href="https://api.whatsapp.com/send?phone=542234060274" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
-                            <p className="footer-info">223-4060274</p></a>
-                    </div>
-                </div>
-            </div>
+            {/* {showImageModal && (
+            // <div className="modal-overlay" onClick={closeModal}>
+            //     <div className="modal-content">
+            //         <img src={ModalHome} alt="" className="modal-image"/>
+            //     <p className="modal-title">Estimado usuario: le informamos que incorporamos los siguientes números telefónicos para el envío de facturas e información institucional por WhatsApp 
+            //     </p>
+            //         <div>
+            //             <a href="https://api.whatsapp.com/send?phone=542233060278" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+            //                 <p className="footer-info">223-3060278</p></a>
+            //                     <a href="https://api.whatsapp.com/send?phone=542233060273" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+            //                 <p className="footer-info">223-3060273</p></a>
+            //                     <a href="https://api.whatsapp.com/send?phone=542234060274" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+            //                 <p className="footer-info">223-4060274</p></a>
+            //         </div>
+            //     </div>
+            // </div>
            
             //  <div className="modal-overlay" onClick={closeModal}>
             //     <div className="modal-content">
@@ -61,7 +61,7 @@ const Home = () => {
             //     </p>
             //     </div>
             // </div>
-        )}
+        )} */}
             <HomePortada2/>
             <HomeServices3/>
             <HomeGuardia2/>
