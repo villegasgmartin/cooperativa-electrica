aecba7187c3762e392308845dd8e2c2732682d56
MartinLuxury <166442590+MartinLuxury@users.noreply.github.com>
2026-05-21 15:23:49 +0200

    popup alerta

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -17,51 +17,58 @@ const Home = () => {
 
     const [showImageModal, setShowImageModal] = useState(false);
 
-    // useEffect(() => {
-    //     const hasShownAlert = sessionStorage.getItem("homeAlertShown");
+    useEffect(() => {
+        const hasShownAlert = sessionStorage.getItem("homeAlertShown");
 
-    //     if (!hasShownAlert) {
-    //         const timer = setTimeout(() => {
-    //             setShowImageModal(true);
-    //             sessionStorage.setItem("homeAlertShown", "true");
-    //         }, 3000);
-    //         return () => clearTimeout(timer);
-    //     }
-    // }, []);
+        if (!hasShownAlert) {
+            const timer = setTimeout(() => {
+                setShowImageModal(true);
+                sessionStorage.setItem("homeAlertShown", "true");
+            }, 3000);
+            return () => clearTimeout(timer);
+        }
+    }, []);
 
-    // const closeModal = () => {
-    //     setShowImageModal(false);
-    // };
+    const closeModal = () => {
+        setShowImageModal(false);
+    };
 
 
     return (
         <div>
             
-            {/* {showImageModal && (
-            // <div className="modal-overlay" onClick={closeModal}>
-            //     <div className="modal-content">
-            //         <img src={ModalHome} alt="" className="modal-image"/>
-            //     <p className="modal-title">Estimado usuario: le informamos que incorporamos los siguientes números telefónicos para el envío de facturas e información institucional por WhatsApp 
-            //     </p>
-            //         <div>
-            //             <a href="https://api.whatsapp.com/send?phone=542233060278" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
-            //                 <p className="footer-info">223-3060278</p></a>
-            //                     <a href="https://api.whatsapp.com/send?phone=542233060273" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
-            //                 <p className="footer-info">223-3060273</p></a>
-            //                     <a href="https://api.whatsapp.com/send?phone=542234060274" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
-            //                 <p className="footer-info">223-4060274</p></a>
-            //         </div>
-            //     </div>
-            // </div>
+            {showImageModal && (
+                <>
+            <div className="modal-overlay" onClick={closeModal}>
+                <div className="modal-content">
+                    <img src={ModalHome} alt="" className="modal-image"/>
+                <p className="modal-title">⚠️ El canal de Whatsapp 54 9 2235405430, correspondiente a la atención comercial del Servicio Eléctrico, se encuentra temporalmente fuera de servicio.
+                    <br />
+                    <br />
+                    Estamos trabajando para reestablecerlo lo antes posible. 
+                    <br />
+                    <br />Recibimos consultas y gestiones a través de nuestro correo electrónico Recibimos consultas y gestiones a través de nuestro correo electrónico comercialgc@cooperativamdp.com.ar o telefónicamente, al 0 800 333 0357
+Pedimos disculpas por las molestias.
+                </p>
+                
+                    <div>
+                                {/* <a href="https://api.whatsapp.com/send?phone=542233060273" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+                            <p className="footer-info">223-3060273</p></a>
+                                <a href="https://api.whatsapp.com/send?phone=542234060274" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+                            <p className="footer-info">223-4060274</p></a> */}
+                    </div>
+                </div>
+            </div>
            
-            //  <div className="modal-overlay" onClick={closeModal}>
-            //     <div className="modal-content">
-            //         <img src={ModalHome} alt="" className="modal-image"/>
-            //     <p className="modal-title"> El martes 18 de noviembre la Cooperativa permanecerá cerrada por motivo del aniversario institucional. Celebramos 91 años de servicio. Muchas gracias.
-            //     </p>
-            //     </div>
-            // </div>
-        )} */}
+             {/* <div className="modal-overlay" onClick={closeModal}>
+                <div className="modal-content">
+                    <img src={ModalHome} alt="" className="modal-image"/>
+                <p className="modal-title"> El martes 18 de noviembre la Cooperativa permanecerá cerrada por motivo del aniversario institucional. Celebramos 91 años de servicio. Muchas gracias.
+                </p>
+                </div>
+            </div> */}
+            </>
+        )}
             <HomePortada2/>
             <HomeServices3/>
             <HomeGuardia2/>
