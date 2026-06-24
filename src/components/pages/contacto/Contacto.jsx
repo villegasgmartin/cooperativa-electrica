dbd22d08ab881d68eec71d6fa87e31fe581a5277
villegasgmartin <villegasgmartin@gmail.com>
2026-06-24 17:11:43 +0200

    borrar guardia

--- src/components/pages/contacto/Contacto.jsx
+++ src/components/pages/contacto/Contacto.jsx
@@ -95,14 +95,14 @@ const Contacto = () => {
       </Helmet>
         <div className='contacto-container'> 
           <div className='contacto-mensaje-container'> 
-            <Fade triggerOnce={true} duration={800} delay={300} direction='left'>
+            {/* <Fade triggerOnce={true} duration={800} delay={300} direction='left'>
               <div className='contacto-mensaje-electrica'>
                 <h3 className='contacto-number'>Guardia técnica Servicio Eléctrico</h3>
                 <p className='contacto-mensaje'>Solo para EMERGENCIAS (Corresponde a casos de Personas Electrodependientes)</p>
                 <h3 className='contacto-number'><a href="https://wa.link/583s3n" target="_blank" rel="noopener noreferrer"> 223 540-5430</a></h3>
                 
               </div>
-            </Fade>
+            </Fade> */}
             <Fade triggerOnce={true} duration={800} delay={300} direction='right'>
                 <div className='contacto-mensaje-nave'>
                   <h3 className='contacto-number'>Guardia técnica NAVE Internet</h3>
